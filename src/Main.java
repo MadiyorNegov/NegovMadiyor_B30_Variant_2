c4a71331a82be292ab2b718bb4ba4cc5212ac709
MadiyorNegov <madiyor033@gmail.com>
2024-04-24 11:43:33 +0500

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        //uyuyutyrrdesssss
-        System.out.println("Hello world!");
+
+
+        String[] str = {"cd", "ac", "dc", "ca", "zz"};
+
+        int count = 0;
+        for (int i = 0; i < str.length - 1; i++) {
+            for (int j = i + 1; j < str.length; j++) {
+                if (Objects.equals(str[i], reverse(str[j]))) {
+                    count++;
+                }
+            }
+
+        }
+        System.out.println(count);
     }
+    public static String reverse(String str) {
+
+        StringBuilder newStr = new StringBuilder();
+        for (int i = str.length() - 1; i > -1; i--) {
+            newStr.append(str.charAt(i));
+        }
+        return new String(newStr);
+    }
+
+
+
+
+
 }
